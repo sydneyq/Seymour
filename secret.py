591d7692544660c5809baccf7fd0fde196280fc7
sydneyq <qrsydney@gmail.com>
2020-09-02 22:34:41 -0400

    changing env vars to int

--- secret.py
+++ secret.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-SYD_ID = os.getenv("SYD_ID")
-OLIVE_ID = os.getenv("OLIVE_ID")
-SEYMOUR_ID = os.getenv("SEYMOUR_ID")
+SYD_ID = int(os.getenv("SYD_ID"))
+OLIVE_ID = int(os.getenv("OLIVE_ID"))
+SEYMOUR_ID = int(os.getenv("SEYMOUR_ID"))
 DB_KEY = os.getenv("DB_KEY")
