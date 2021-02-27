0587f7a0fef0205e9fd67b051b0a4bd3e3a75129
sydneyq <qrsydney@gmail.com>
2021-02-27 01:37:35 -0500

    adding prompts

--- cogs/picknic.py
+++ cogs/picknic.py
@@ -59,8 +59,10 @@ class Picknic(commands.Cog):
                        'interests': '', 'limits': '', 'details': '', 'active': True,
                        'yes': [], 'no': []}
             self.dbConnection.insertPicknic(profile)
-
         return profile
+
+    def edit_picknic(self, member: discord.Member, key, value):
+        profile = self.dbConnection.updatePicknic({"id": str(member.id)}, {"$set": {key: value}})
 
     def picknic_does_exist(self, _id):
         profile = self.dbConnection.findPicknic({"id": str(_id)})
@@ -101,7 +103,7 @@ class Picknic(commands.Cog):
             return
         await ctx.send(embed=self.get_picknic_embed_from_member(member))
 
-    @commands.command(aliases=['spn'])
+    @commands.command(aliases=['spn', 'picknicmenu'])
     async def startpicknic(self, ctx):
         # for now, limit to testers
         if not self.meta.isBotOwner(ctx.author):
@@ -132,26 +134,84 @@ class Picknic(commands.Cog):
         except asyncio.TimeoutError:
             await msg.edit(embed=self.meta.embedOops("Picknic menu timed out. You took too long to reply!"))
             return
-        else:
-            # create profile
-            if emoji == '🏕':
-                pass
-            # edit profile
-            elif emoji == '♻':
-                pass
-            # report profile
-            elif emoji == '⚠':
-                pass
-            # reset swipes
-            elif emoji == '🍐':
-                pass
-            # flip through profile
-            elif emoji == '🛰':
-                if not isinstance(ctx.channel, discord.DMChannel):
-                    await ctx.send(embed=self.meta.embedOops('Try using this command in a private message to me!'))
-                    return
-                else:
-                    pass
+
+        await msg.clear_reactions()
+
+        # create profile
+        if emoji == '🏕':
+            if self.picknic_does_exist(ctx.author().id):
+                await msg.edit(embed=self.meta.embedOops('You already have a Picknic profile! '
+                                                         'Try going back to the menu and editing it.'))
+                return
+            else:
+                await msg.edit(embed=self.meta.embed('First, are you over 18 years of age? ',
+                                                     'Users who are found or suspected to be '
+                                                     'underage will be banned from the system.'
+                                                     'By using our platform and space, you agree to '
+                                                     'follow our rules, including following Discord ToS.'))
+
+                await msg.edit(embed=self.meta.embed('What would you like to be named?'))
+                await msg.edit(embed=self.meta.embed('What are your pronouns?'))
+
+                await msg.edit(embed=self.meta.embed('What gender label(s) fit you best? '
+                                                     'Please type the numbers separated by commas.',
+                                                     "`[1]` Male\n"
+                                                     "`[2]` Female\n"
+                                                     "`[3]` Non-binary\n"
+                                                     "`[4]` Agender\n"
+                                                     "`[5]` Genderfluid"
+                                                     "`[6]` Prefer not to specify"))
+
+                await msg.edit(embed=self.meta.embed('What gender labels are you looking for?'))
+                await msg.edit(embed=self.meta.embed('What role(s) do you fit?'))
+                await msg.edit(embed=self.meta.embed('What role(s) are you looking for (LF)?'))
+                await msg.edit(embed=self.meta.embed('What mediums do you use for hypnosis?'))
+                await msg.edit(embed=self.meta.embed('What term(s) are you looking for (LF)?'))
+                await msg.edit(embed=self.meta.embed('Are you open to NSFW partners?'))
+                await msg.edit(embed=self.meta.embed("Type what you'd like your interests on your profile to say."))
+                await msg.edit(embed=self.meta.embed("Type what you'd like your limits on your profile to say."))
+                await msg.edit(embed=self.meta.embed("Type what you'd like your details on your profile to say."))
+                await msg.edit(embed=self.meta.embed("And we're done!",
+                                                     "Feel free to check out your profile using the `;pn` command."))
+                return
+        # edit profile
+        elif emoji == '♻':
+            # check that the user has a profile to edit
+            if not self.picknic_does_exist(ctx.author.id):
+                await msg.edit(embed=self.meta.embedOops("You don't have a Picknic profile yet! "
+                                                         "Try going back to the menu to create one."))
+                return
+
+            title = "What would you like to edit?"
+            desc = "`[1]` Enable/Disable my profile." \
+                   "`[2]` Change my name." \
+                   "`[3]` Change my gender(s)." \
+                   "`[4]` Change my pronoun(s)." \
+                   "`[5]` Change my role(s)." \
+                   "`[6]` Change my medium(s)." \
+                   "`[7]` Toggle my profile as NSFW/SFW." \
+                   "`[8]` Change the gender(s) I'm looking for." \
+                   "`[9]` Change the role(s) I'm looking for." \
+                   "`[10]` Change my interest(s)." \
+                   "`[11]` Change my limit(s)." \
+                   "`[12]` Change my detail(s)." \
+                   "`[13]` Change the term(s) I'm looking for."
+            return
+        # report profile
+        elif emoji == '⚠':
+            return
+        # reset swipes
+        elif emoji == '🍐':
+            return
+        # flip through profile
+        elif emoji == '🛰':
+            # check that the requesting user has a profile themselves first
+            # check that it's in dms
+            if not isinstance(ctx.channel, discord.DMChannel):
+                await ctx.send(embed=self.meta.embedOops('Try using this command in a private message to me!'))
+                return
+            else:
+                return
 
 
 def setup(client):
